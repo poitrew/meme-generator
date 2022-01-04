132f86d164eb397276ea02f76330f0f3592e04f1
Poitrew <dtdat101101@gmail.com>
2022-01-04 23:34:04 +0700

    update meme text importation to meme image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -7,7 +7,7 @@ export default function Meme() {
     const [memeImage, setMemeImage] = React.useState({
         topText: "",
         bottomText: "",
-        url: ""
+        url: "http://i.imgflip.com/1bij.jpg"
     })
 
     const [allMemeImages, setAllMemeImages] = React.useState(memesData)
@@ -23,14 +23,28 @@ export default function Meme() {
         )) 
     }
 
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMemeImage(prev => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
     return (
         <main>
             <div className="form">
-                <input type="text" className="form--input" placeholder="Top text"/>
-                <input type="text" className="form--input" placeholder="Bottom text" />
+                <input type="text" className="form--input" placeholder="Top text" onChange={handleChange} value={memeImage.topText} name="topText"/>
+                <input type="text" className="form--input" placeholder="Bottom text" onChange={handleChange} value={memeImage.bottomText} name="bottomText" />
                 <button className="form--btn" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
-            <img src={memeImage.url} alt="random meme" className="meme-img"></img>
+            <div className="meme">
+                <img src={memeImage.url} alt="random meme" className="meme--img"></img>
+                <h2 className="meme--text top">{memeImage.topText}</h2>
+                <h2 className="meme--text bottom">{memeImage.bottomText}</h2>
+            </div>
         </main>
     )
 }
